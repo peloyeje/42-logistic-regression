f398fa937c8ea5be8ae3d656197d1c7567ed8c16
Jean-Eudes Peloye <hello@peloye.je>
2018-10-10 19:15:38 +0200

    Add OneHotEncoder class

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class OneHotEncoder:
+    def __init__(self):
+        self.categories = []
+        pass
+
+    def fit(self, X):
+        """"""
+        self.categories = np.unique(X)
+        return X
+
+    def transform(self, X):
+        """"""
+        return np.array(
+            [(X == k).astype('int') for k in self.categories]
+        ).T
+
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
